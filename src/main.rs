ce9152bf5faf4dbd1ee77ca2818310b05ff562fc
José Fco. Rives <jose@digio.es>
2019-09-02 20:45:08 +0200

    changed library for getting public ip

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+extern crate my_internet_ip;
+
 fn main() {
     for iface in get_if_addrs::get_if_addrs().unwrap() {
         println!("{}: {}", iface.name, iface.ip());
     }
-    println!("Public IP: {}", my_public_ip::resolve().unwrap());
+
+    let ip: ::std::net::IpAddr = match my_internet_ip::get() {
+        Ok(ip) => ip,
+        Err(e) => panic!("Could not get IP: {:?}", e),
+    };
+    println!("Public IP: {}", ip);
 }
